69b49d9fffa18f574d02c537f6fe8b3ec40bd4fb
learnwithpushpesh <kumpush868@gmail.com>
2022-04-18 00:31:14 +0200

    added cart test page test

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -4,7 +4,7 @@ public class CartPage {
 	
 public static void main(String[] args) {
 		
-		System.out.println("This is home page");
+		System.out.println("This is cart page");
 
 	}
 
---
+++ src/tests/CartPageTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class CartPageTest {
+	
+public void test1() {
+		
+		System.out.println("test 1- CP");
+	}
+
+
+}
