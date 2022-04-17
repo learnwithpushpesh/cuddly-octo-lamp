8be88cdac6af15b50b2b175d74c8684968f3515c
learnwithpushpesh <kumpush868@gmail.com>
2022-04-17 20:29:12 +0200

    added/updated/deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -5,6 +5,12 @@ public class LoginPage {
 	public static void main(String[] args) {
 		
 		System.out.println("This is login page");
+		
+		String un = "admin";
+		
+		String password = "password";
+		
+		System.out.println("Logged in");
 	}
 
 }
