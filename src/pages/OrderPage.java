9426691a9be8aa4a7608422896caf4c5d3f9c776
Chena Srika <chenasrika00@gmail.com>
2022-04-18 20:16:42 +0200

    updated order page logic as per review comments

--- src/pages/OrderPage.java
+++ src/pages/OrderPage.java
@@ -4,6 +4,7 @@ public class OrderPage {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from OrderPage");
+		System.out.println("Hello from OrderPage, logic added");
 	}
 
 }
--- src/tests/OrderPageTest.java
+++ src/tests/OrderPageTest.java
@@ -5,6 +5,8 @@ public class OrderPageTest {
 public void test1() {
 		
 		System.out.println("test 1- OP");
+		System.out.println("Hello from OrderPage, test logic added.");
+		
 	}
 
 
