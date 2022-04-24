5ed9a6d9d4fe045b1c158461bb73a1cfa722edbb
learnwithpushpesh <kumpush868@gmail.com>
2022-04-24 17:11:46 +0200

    added classic page from local

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class ClassicPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n1 = 10;
+		int n2 = 20;
+		
+		int res = n1+n2;
+		System.out.println(res);
+
+	}
+
+}
